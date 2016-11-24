32bd5c2d1dc9d0a4843eebc1af95ca71c9183ddc
Molly Thomson <mthomson1196@gmail.com>
2016-11-24 08:18:48 -0500

    modified home

--- geocrypto/home.php
+++ geocrypto/home.php
@@ -82,50 +82,6 @@
 				
 				<input type="submit" value="Upload">
 			</form>
-		
-		
-		
-		
-		<!-- Getting the location -->
-        <p>Click the button to get your coordinates.</p>
-
-        <button onclick="getLocation()">Get Location!</button>
-
-        <p id="demo"></p>
-
-        <script>
-        var x = document.getElementById("demo");
-        function getLocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(showPosition, showError);
-            } else { 
-                x.innerHTML = "Geolocation is not supported by this browser.";
-            }
-        }
-        function showPosition(position) {
-            x.innerHTML = "Latitude: " + position.coords.latitude + 
-            "<br>Longitude: " + position.coords.longitude;
-        }
-        function showError(error) {
-            switch(error.code) {
-                case error.PERMISSION_DENIED:
-                    x.innerHTML = "User denied the request for Geolocation."
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    x.innerHTML = "Location information is unavailable."
-                    break;
-                case error.TIMEOUT:
-                    x.innerHTML = "The request to get user location timed out."
-                    break;
-                case error.UNKNOWN_ERROR:
-                    x.innerHTML = "An unknown error occurred."
-                    break;
-            }
-        }
-        </script>
-
-    </div>
-    <!-- /.container -->
 
     <!-- jQuery Version 1.11.1 -->
     <script src="js/jquery.js"></script>
@@ -136,5 +92,3 @@
 </body>
 
 </html>
-Contact GitHub API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security Status Help
